ff7605e0c226bd70908cc98e9f8e08766ec06cc9
Ranjeet Vishwakarma <rv238502@gmail.com>
2024-07-31 16:30:32 +0530

    useRef Component Added

--- src/component/UseRef.tsx
+++ src/component/UseRef.tsx
@@ -1,15 +1,27 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const UseRef = () => {
+
     const ref = useRef(0);
-    const handleClick = () => { 
+    const divRef = useRef(null);
+
+    const handleClick = () => {
         ref.current += 1;
         console.log('Current value of ref is:', ref.current);  // Output: Current value of ref is: 1
     }
+    useEffect(() => {
+        console.log('Component Mounted');
+        // divRef.current.style.color = 'red';
+        console.log(divRef.current);
+        
+
+    })
+
     return (
         <>
             <h1>UseRef Example --- </h1>
             <button onClick={handleClick}>Click me for Action</button>
+            <h2 ref={divRef}>This is Second Heading of the Website</h2>
 
         </>
     )
